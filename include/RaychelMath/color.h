26a17441fce8e6253ef6bfabd72780d8cd3a25e6
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-03-17 17:39:41 +0100

    formatting

--- include/RaychelMath/color.h
+++ include/RaychelMath/color.h
@@ -31,14 +31,14 @@
 #include "Tuple.h"
 
 #include <limits>
-#include <ratio>
 #include <numeric>
+#include <ratio>
 namespace Raychel {
 
     struct ColorTag
     {};
 
-    template<Arithmetic T>
+    template <Arithmetic T>
     using color = Tuple<T, 3, ColorTag>;
 
     template <Arithmetic T>
@@ -50,7 +50,7 @@ namespace Raychel {
         return a;
     }
 
-    template<Arithmetic T>
+    template <Arithmetic T>
     constexpr color<T> operator*(const color<T>& a, const color<T>& b)
     {
         auto res{a};
@@ -58,15 +58,14 @@ namespace Raychel {
         return res;
     }
 
-    template<Arithmetic T>
+    template <Arithmetic T>
     constexpr T brightness(const color<T>& c)
     {
         return (c[0] + c[1] + c[2]) / 3;
     }
 
-    namespace details
-    {
-        template<std::integral From, std::integral To>
+    namespace details {
+        template <std::integral From, std::integral To>
         constexpr color<To> convert_color_helper(const color<From>& c)
         {
             using Int = std::common_type_t<From, To>;
@@ -74,22 +73,22 @@ namespace Raychel {
             constexpr auto to_max = std::numeric_limits<To>::max();
             constexpr auto from_max = std::numeric_limits<From>::max();
 
-            if constexpr(to_max > from_max) {
+            if constexpr (to_max > from_max) {
                 constexpr To ratio = to_max / from_max;
                 return color<To>{static_cast<To>(c[0]) * ratio, static_cast<To>(c[1]) * ratio, static_cast<To>(c[2]) * ratio};
             }
             //TODO: find a better way to dodge lossy floating point arithmetic
             constexpr auto ratio = static_cast<double>(to_max) / from_max;
             return color<To>{static_cast<To>(c[0] * ratio), static_cast<To>(c[1] * ratio), static_cast<To>(c[2] * ratio)};
-       }
+        }
 
-        template<std::floating_point From, std::floating_point To>
+        template <std::floating_point From, std::floating_point To>
         constexpr color<To> convert_color_helper(const color<From>& c)
         {
             return color<To>{c[0], c[1], c[2]};
         }
 
-        template<std::floating_point From, std::integral To>
+        template <std::floating_point From, std::integral To>
         constexpr color<To> convert_color_helper(const color<From>& c)
         {
             using std::clamp;
@@ -97,37 +96,36 @@ namespace Raychel {
             return color<To>{clamp<From>(c[0], 0, 1) * max, clamp<From>(c[1], 0, 1) * max, clamp<From>(c[2], 0, 1) * max};
         }
 
-        template<std::integral From, std::floating_point To>
+        template <std::integral From, std::floating_point To>
         constexpr color<To> convert_color_helper(const color<From>& c)
         {
             constexpr auto max = static_cast<To>(std::numeric_limits<From>::max());
             return color<To>{static_cast<To>(c[0]) / max, static_cast<To>(c[1]) / max, static_cast<To>(c[2]) / max};
         }
-    }
+    } // namespace details
 
-
-    template<Arithmetic To, std::convertible_to<To> From>
+    template <Arithmetic To, std::convertible_to<To> From>
     constexpr color<To> convert_color(const color<From>& c)
     {
-        if constexpr(std::is_same_v<From, To>) {
+        if constexpr (std::is_same_v<From, To>) {
             return c;
         }
         return details::convert_color_helper<From, To>(c);
     }
 
-    template<Arithmetic T>
+    template <Arithmetic T>
     constexpr color<T> color_from_rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b)
     {
         return convert_color<T>(color<std::uint8_t>{r, g, b});
     }
 
-    template<Arithmetic T>
+    template <Arithmetic T>
     constexpr color<T> color_from_hex(std::uint32_t hex)
     {
         using u8 = std::uint8_t;
-        const u8 r = (hex >> 16)    & 0xFFU;
-        const u8 g = (hex >> 8)     & 0xFFU;
-        const u8 b =  hex           & 0xFFU;
+        const u8 r = (hex >> 16) & 0xFFU;
+        const u8 g = (hex >> 8) & 0xFFU;
+        const u8 b = hex & 0xFFU;
 
         return convert_color<T>(color<u8>{r, g, b});
     }
