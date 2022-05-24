0e41aca5e4e6c48b5d3e3c8dc00b4c61961778ed
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-05-24 23:22:06 +0200

    Concepts: fixed fatal syntax error
    Turns out just stating the name of a concept does not check if it is satisfied. Thanks to clang-tidy for spotting this bug

--- include/RaychelMath/concepts.h
+++ include/RaychelMath/concepts.h
@@ -3,7 +3,7 @@
 * \author Weckyy702 (weckyy702@gmail.com)
 * \brief Math-related concepts
 * \date 2021-08-30
-* 
+*
 * MIT License
 * Copyright (c) [2021] [Weckyy702 (weckyy702@gmail.com | https://github.com/Weckyy702)]
 * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -12,10 +12,10 @@
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,7 +23,7 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
-* 
+*
 */
 #ifndef RAYCHEL_MATH_CONCEPTS_H
 #define RAYCHEL_MATH_CONCEPTS_H
@@ -31,6 +31,8 @@
 #include <concepts>
 #include <random>
 #include <type_traits>
+
+// clang-format off
 
 namespace Raychel {
 
@@ -42,12 +44,12 @@ namespace Raychel {
         template <typename P, typename D>
         concept StdRandomNumberDistributionParamType = requires(P p)
         {
-            std::is_copy_constructible_v<P>;
-            std::is_copy_assignable_v<P>;
-            std::equality_comparable<P>;
+            requires std::is_copy_constructible_v<P>;
+            requires std::is_copy_assignable_v<P>;
+            requires std::equality_comparable<P>;
 
             typename P::distribution_type;
-            std::same_as<typename P::distribution_type, D>;
+            requires std::same_as<typename P::distribution_type, D>;
         };
 
     } // namespace details
@@ -67,12 +69,12 @@ namespace Raychel {
     template <typename E>
     concept StdRandomNumberEngine = requires(E e, const E x, unsigned long long z, std::ostream& os, std::istream& is)
     {
-        std::uniform_random_bit_generator<E>;
+        requires std::uniform_random_bit_generator<E>;
 
         typename E::result_type;
 
         E();
-        std::is_constructible_v<E, typename E::result_type>;
+        requires std::is_constructible_v<E, typename E::result_type>;
         E(x);
         E(e);
         //E(q) with q satisfying SeedSequence
@@ -95,16 +97,16 @@ namespace Raychel {
     template <typename D>
     concept StdRandomNumberDistribution = requires(D d, const D x, std::ostream& os, std::istream& is)
     {
-        std::is_copy_constructible_v<D>;
-        std::is_copy_assignable_v<D>;
+        requires std::is_copy_constructible_v<D>;
+        requires std::is_copy_assignable_v<D>;
 
         typename D::result_type;
         typename D::param_type;
 
-        details::StdRandomNumberDistributionParamType<typename D::param_type, D>;
+        requires details::StdRandomNumberDistributionParamType<typename D::param_type, D>;
 
         D();
-        std::is_constructible_v<D, typename D::param_type>;
+        requires std::is_constructible_v<D, typename D::param_type>;
 
         { d.reset() } -> details::Void;
         { x.param() } -> std::same_as<typename D::param_type>;
